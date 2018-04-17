e9b4bbee6d900a1551a84323050eb852240de26f
Neutrollized <glen.yu@gmail.com>
2018-04-17 08:29:23 -0400

    Added minimize.sh to clean swap at the end

---
+++ scripts/minimize.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# This script minimizes the size of images.
+# It should be run on non-Docker, base Linux images.
+set -eux
+
+SWAPUUID="$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)"
+case "$?" in
+    2|0) ;;
+    *) exit 1 ;;
+esac
+set -e
+
+echo '==> Clear out swap and disable until reboot'
+if [ "x${SWAPUUID}" != "x" ]; then
+    # Whiteout the swap partition to reduce box size
+    # Swap is disabled till reboot
+    SWAPPART="$(readlink -f "/dev/disk/by-uuid/${SWAPUUID}")"
+    /sbin/swapoff "${SWAPPART}"
+    dd if=/dev/zero of="${SWAPPART}" bs=1M || echo "dd exit code $? is suppressed"
+    /sbin/mkswap -U "${SWAPUUID}" "${SWAPPART}"
+fi
+
+dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed"
+rm -f /EMPTY
+# Block until the empty file has been removed, otherwise, Packer
+# will try to kill the box while the disk is still full and that's bad
+sync
