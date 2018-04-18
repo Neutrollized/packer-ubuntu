739958c41ea148898a883b1506eec1a34ca8359a
Glen Yu <Glen.Yu@Cantire.com>
2018-04-18 09:39:15 -0400

    removed apt-get remove linux-headers-generic

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -eux
 
-apt-get remove -y linux-headers-generic
-
-apt-get install -y linux-headers-virtual virtualbox-guest-dkms virtualbox-guest-utils
+apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils linux-headers-virtual
