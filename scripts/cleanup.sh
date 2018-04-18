41214e2d941ba9212fc128cd81a49944dee0b97a
Glen Yu <Glen.Yu@Cantire.com>
2018-04-18 11:17:18 -0400

    added /var/log cleanup to cleanup.sh script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,3 +6,10 @@ apt-get autoremove --purge -y
 
 # clean apt cache
 apt-get clean
+
+# clean up /var/log"
+find /var/log/ -type f -exec /bin/sh -c '>{}' \;
+rm -f /var/log/*.old
+rm -f /var/log/*.log
+rm -f /var/log/*.log.*
+rm -f /var/log/*.syslog
